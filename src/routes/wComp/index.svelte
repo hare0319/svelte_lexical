2353f3cd8f6a24fbf56ff02a67db5378caa22154
Harenc <hare0319@gmail.com>
2022-06-28 17:47:33 +0800

    style: :lipstick: copy example theme and styles from react wrapper example

---
+++ src/routes/wComp/index.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import LexicalComposer from "$lib/Lexical/LexicalComposer.svelte";
+  import ContentEditable from "$lib/Lexical/LexicalContentEditable.svelte";
+  import PlainTextPlugin from "$lib/Lexical/LexicalPlainTextPlugin.svelte";
+  import theme from "./_theme/ExampleTheme";
+
+  import "$lib/style/styles.css";
+
+  const initialConfig = {
+    namespace: "MyEditor",
+    theme,
+    onError: (error: Error) => console.error(error),
+  };
+</script>
+
+<div class="App">
+  <h1>Lexical editor with composer</h1>
+
+  <LexicalComposer {initialConfig}>
+    <div class="editor-container">
+      <PlainTextPlugin>
+        <ContentEditable slot="contentEditable" class="editor-input" />
+        <div slot="placeholder" class="editor-placeholder">
+          Enter some plain text...
+        </div>
+      </PlainTextPlugin>
+    </div>
+  </LexicalComposer>
+</div>
