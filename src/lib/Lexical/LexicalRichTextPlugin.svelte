26faa8297f5f5c430dded8cf3f88ff6fa0b56143
Harenc <hare0319@gmail.com>
2022-06-29 17:45:56 +0800

    feat: :sparkles: Support RichTextPlugin for Lexical-svelte wrapper

---
+++ src/lib/Lexical/LexicalRichTextPlugin.svelte
@@ -0,0 +1,31 @@
+<script lang="ts" Context="Module">
+  import { getLexicalComposerContext } from "$lib/Lexical/LexicalComposerContext";
+  import { registerRichText } from "@lexical/rich-text";
+  import { getCanShowPlaceholder } from "$lib/shared/useCanShowPlaceholder";
+  import { getDecorators } from "$lib/shared/useDecorators";
+  import { onMount } from "svelte";
+
+  const [editor] = getLexicalComposerContext();
+
+  let showPlaceholder: boolean = getCanShowPlaceholder(editor, (stat) => {
+    showPlaceholder = stat;
+  });
+
+  // Decorator not fully supported, as limited resource available
+  // and not required for now.
+  let decorators = getDecorators(editor, (decs) => {
+    decorators = decs;
+  });
+
+  onMount(() => {
+    registerRichText(editor);
+  });
+
+  // export let contentEditable: HTMLElement;
+  // export let placeholder: HTMLElement | string;
+</script>
+
+<slot name="contentEditable" />
+{#if showPlaceholder}
+  <slot name="placeholder" />
+{/if}
