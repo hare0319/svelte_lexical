a75b078fa90d5b50623366acd1fb9a7d09c8083f
Harenc <hare0319@gmail.com>
2022-06-29 17:42:43 +0800

    feat: :triangular_flag_on_post: Added a Lex page to demo that the original way of integrating lexical and svelte, leads to different component instances share the same editor instance
    Originally, attempted to integrate lexical and svelte directly with JS functions, which create editor instance in top level of a component module. This leads to different component instances share the same editor instance

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -18,6 +18,9 @@
       <li class:active={$page.url.pathname === "/richText"}>
         <a href="/richText">Rich</a>
       </li>
+      <li class:active={$page.url.pathname === "/Lex"}>
+        <a href="/Lex">Lex</a>
+      </li>
     </ul>
     <svg viewBox="0 0 2 3" aria-hidden="true">
       <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
---
+++ src/routes/Lex.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Lexical from "$lib/components/Lexical/lexical.svelte";
+</script>
+
+<h1>Lexical Instance</h1>
+
+<Lexical />
